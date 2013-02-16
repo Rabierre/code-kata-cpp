52950c28f2652e31496a4a94e500ae4569466593
rabierre <rabierre@gmail.com>
2013-02-16 16:37:42 +0900

    move dir

---
+++ code-kata-cpp/UniqueCharSequence.h
@@ -0,0 +1,20 @@
+//
+//  UniqueCharSequence.h
+//  code-kata-cpp2
+//
+//  Created by 서 지혜 on 13. 2. 12..
+//  Copyright (c) 2013년 rabierre. All rights reserved.
+//
+
+#ifndef __code_kata_cpp2__UniqueCharSequence__
+#define __code_kata_cpp2__UniqueCharSequence__
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isUniqueAsciiChars(string chars);
+bool isUniqueUnicodeChars(string chars);
+
+#endif /* defined(__code_kata_cpp2__UniqueCharSequence__) */
